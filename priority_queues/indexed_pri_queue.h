28fbaeeb9d47576d330956200e874cc9399eb50c
amukherj <arindam.mukerjee@gmail.com>
2021-11-08 01:06:37 +0530

    Add the IndexedPriorityQueue class.
    Supports only basic push, pop, and querying by name.

---
+++ priority_queues/indexed_pri_queue.h
@@ -0,0 +1,132 @@
+#ifndef INDEXED_PRI_QUEUE
+#define INDEXED_PRI_QUEUE
+
+#include <bits/stdc++.h>
+
+namespace fontus {
+
+template <typename T, typename U = int>
+class IndexedPriorityQueue {
+public:
+	IndexedPriorityQueue(int initial_size) {
+		id_to_key.reserve(initial_size);
+		heap.reserve(initial_size);
+		id_to_pos.reserve(initial_size);
+		pos_to_id.reserve(initial_size);
+	}
+
+	bool push(const T& key, U value) {
+		if (key.empty() || !value) {
+			return false;
+		}
+
+		int key_id = 0;
+		if (id_pool.begin() == id_pool.end()) {
+			key_id = heap.size();
+		} else {
+			key_id = *id_pool.begin();
+			id_pool.erase(id_pool.begin());
+		}
+		key_to_id.insert({key, key_id});
+		if (key_id >= id_to_key.size()) {
+			// This happens when we have not handed this key_id out earlier
+			id_to_key.push_back(key);
+			assert(id_to_key.size() == key_id + 1);
+		} else {
+			// This happens when we reuse a key_id from the pool
+			id_to_key[key_id] = key;
+		}
+		heap.push_back(value);
+		id_to_pos[key_id] = heap.size() - 1;
+		pos_to_id[heap.size() - 1] = key_id;
+
+		for (int i = heap.size() - 1; i > 0; i /= 2) {
+			if (heap[i] <= heap[i/2]) {
+				break;
+			}
+			heap_swap(i, i/2);
+		}
+		return true;
+	}
+
+	std::optional<std::pair<T, U>> pop() {
+		if (heap.empty()) {
+			return std::nullopt;
+		}
+		int key_id = pos_to_id[0];
+		int priority = heap[0];
+		T key = id_to_key[key_id];
+
+		heap[0] = heap.back();
+		heap.pop_back();
+		pos_to_id[0] = pos_to_id[heap.size()];
+		id_to_pos[pos_to_id[0]] = 0;
+
+		for (int i = 0; 2*i + 1 < heap.size(); ) {
+			if (heap[i] >= heap[2*i + 1] && (heap.size() <= 2*i + 2
+				|| heap[i] >= heap[2*i + 2])) {
+				break;
+			}
+			int j = 2*i + 2;
+			if (j >= heap.size() || heap[j-1] > heap[j]) {
+				j = j-1;
+			}
+			heap_swap(i, j);
+			i = j;
+		}
+		id_pool.insert(key_id);
+		id_to_key[key_id] = "";
+		key_to_id.erase(key);
+
+		return std::make_pair(key, priority);
+	}
+
+	std::optional<std::pair<T, U>> remove(const T& key);
+
+	std::optional<U> query_priority(const T& key) const {
+		auto iter = key_to_id.find(key);
+		if (iter == key_to_id.end()) {
+			return std::nullopt;
+		}
+
+		int key_id = iter->second;
+		assert(id_to_pos[key_id] < heap.size());
+		return heap[id_to_pos[key_id]];
+	}
+
+	void update_priority(const T& key, U value);
+
+private:
+	// map keys to an incremental key id
+	std::unordered_map<T, int> key_to_id;
+
+	// reverse map the key ids to keys
+	std::vector<T> id_to_key;
+
+	// actual heap data structure
+	std::vector<U> heap;
+
+	// map key ids to position in heap
+	std::vector<int> id_to_pos;
+
+	// map position in heap to key ids
+	std::vector<int> pos_to_id;
+
+	// Spare key ids for popped elements
+	// for reuse
+	std::set<int> id_pool;
+
+	void heap_swap(int i, int j) noexcept {
+		std::swap(heap[i], heap[j]);
+		int key_i = pos_to_id[i];
+		int key_j = pos_to_id[j];
+		pos_to_id[i] = key_j;
+		pos_to_id[j] = key_i;
+		id_to_pos[key_j] = i;
+		id_to_pos[key_i] = j;
+	}
+};
+
+} // namespace fontus
+
+#endif /* INDEXED_PRI_QUEUE */
---
+++ priority_queues/ipq_ex1.cc
@@ -0,0 +1,32 @@
+#include "indexed_pri_queue.h"
+
+int main() {
+	fontus::IndexedPriorityQueue<std::string> ipq(10);
+	ipq.push("hello", 1);
+	ipq.push("welcome", 2);
+	ipq.push("good morning", 3);
+	ipq.push("hola", 4);
+	ipq.push("bongiorno", 5);
+	ipq.push("what's up", 6);
+	ipq.push("take care", 7);
+
+	std::cout << "Priority for good morning is "
+		      << ipq.query_priority("good morning").value_or(0) << '\n';
+	std::cout << "Priority for bongiorno is "
+		      << ipq.query_priority("bongiorno").value_or(0) << '\n';
+	std::cout << "Priority for what's up is "
+		      << ipq.query_priority("what's up").value_or(0) << '\n';
+	std::cout << "Priority for welcome is "
+		      << ipq.query_priority("welcome").value_or(0) << '\n';
+	std::cout << "Priority for hello is "
+		      << ipq.query_priority("hello").value_or(0) << '\n';
+	std::cout << "Priority for hola is "
+		      << ipq.query_priority("hola").value_or(0) << '\n';
+	std::cout << "Priority for take care is "
+		      << ipq.query_priority("take care").value_or(0) << '\n';
+
+	for (auto val = ipq.pop(); val; val = ipq.pop()) {
+		std::cout << std::get<0>(val.value_or(
+					std::make_pair(std::string("[]"), 0))) << " popped\n";
+	}
+}
